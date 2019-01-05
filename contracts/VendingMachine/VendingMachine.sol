2c91e2c0dfe7c1463b371fc5db119d9464ee98a4
dbe <davidbrianethier@gmail.com>
2019-01-05 09:43:41 -0700

    Updating the vending machine to handle withdrawls with whitelisting

--- contracts/VendingMachine/VendingMachine.sol
+++ contracts/VendingMachine/VendingMachine.sol
@@ -3,16 +3,40 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/access/roles/MinterRole.sol";
 
-contract ERC20Mintable is ERC20, MinterRole {
-  string public name;
-  string public symbol;
-  uint8 public decimals = 18;
+/**
+ * @title Burnable Token
+ * @dev Token that can be irreversibly burned (destroyed).
+ */
+contract ERC20Burnable is ERC20 {
+    //Call this creator because I don't want to
+    //confuse it with the standard "Ownable" interface which allows for reassignment
+    address creator;
 
-  constructor(string memory _name, string memory _symbol) public {
-      name = _name;
-      symbol = _symbol;
+    constructor() public {
+        creator = msg.sender;
+    }
+
+    modifier onlyCreator() {
+    require(msg.sender == creator);
+    _;
   }
 
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * This is different then the standard Burnable definition.
+     * We only want the vending machine to be able to burn the tokens, but we don't want to require
+     * the two step approve and burnFrom which the standard case would require.
+     * @param from The address of which tokens should be burned away from.
+     * @param value The amount of token to be burned.
+     */
+    function burn(address from, uint256 value) public onlyCreator {
+        _burn(from, value);
+    }
+}
+
+
+contract ERC20Mintable is ERC20, MinterRole {
   /**
    * @dev Function to mint tokens
    * @param to The address that will receive the minted tokens.
@@ -29,6 +53,17 @@ contract ERC20Mintable is ERC20, MinterRole {
   {
     _mint(to, amount);
     return true;
+  }
+}
+
+contract ERC20Vendable is ERC20Mintable, ERC20Burnable {
+  string public name;
+  string public symbol;
+  uint8 public decimals = 18;
+
+  constructor(string memory _name, string memory _symbol) public {
+      name = _name;
+      symbol = _symbol;
   }
 }
 
@@ -72,13 +107,55 @@ contract AdministratorRole {
   }
 }
 
-contract VendingMachine is AdministratorRole {
-  ERC20Mintable public tokenContract;
+contract WhitelistedRole is AdministratorRole {
+    using Roles for Roles.Role;
 
-  event Deposit(address indexed dst, uint wad);
+    event WhitelistedAdded(address indexed account);
+    event WhitelistedRemoved(address indexed account);
+
+    Roles.Role private _whitelisteds;
+
+    modifier onlyWhitelisted() {
+        require(isWhitelisted(msg.sender));
+        _;
+    }
+
+    function isWhitelisted(address account) public view returns (bool) {
+        return _whitelisteds.has(account);
+    }
+
+    function addWhitelisted(address account) public onlyAdministrator {
+        _addWhitelisted(account);
+    }
+
+    function removeWhitelisted(address account) public onlyAdministrator {
+        _removeWhitelisted(account);
+    }
+
+    function renounceWhitelisted() public {
+        _removeWhitelisted(msg.sender);
+    }
+
+    function _addWhitelisted(address account) internal {
+        _whitelisteds.add(account);
+        emit WhitelistedAdded(account);
+    }
+
+    function _removeWhitelisted(address account) internal {
+        _whitelisteds.remove(account);
+        emit WhitelistedRemoved(account);
+    }
+}
+
+contract VendingMachine is AdministratorRole, WhitelistedRole {
+  ERC20Vendable public tokenContract;
+
+  event Deposit(address indexed depositor, uint amount);
+  event Withdraw(address indexed withdrawer, uint amount);
+
 
   constructor() public {
-    tokenContract = new ERC20Mintable("DenDai", "DEN");
+    tokenContract = new ERC20Vendable("DenDai", "DEN");
   }
 
   //Fallback. Just send currency here to deposit
@@ -89,6 +166,13 @@ contract VendingMachine is AdministratorRole {
   function deposit() public payable {
     tokenContract.mint(msg.sender, msg.value);
     emit Deposit(msg.sender, msg.value);
+  }
+
+  function withdraw(uint256 amount) public onlyWhitelisted {
+    tokenContract.burn(msg.sender, amount);
+    msg.sender.transfer(amount);
+
+    emit Withdraw(msg.sender, amount);
   }
 
   //TODO: Decide if we want to trust the admin system enough to allow this form of functionality
