c27f89c6cae84b6336ac619cac772739ba1ff7ab
dbe <davidbrianethier@gmail.com>
2019-01-08 19:20:58 -0700

    Tests working

--- embarkDApp/test/contract_spec.js
+++ embarkDApp/test/contract_spec.js
@@ -35,15 +35,4 @@ contract("VendingMachine", function () {
     let creator = await ERC20Vendable.methods.creator().call();
     expect(creator, "Creator should be the VendingMachine").to.equal(VendingMachine.options.address);
   });
-
-  // it("set storage value", async function () {
-  //   await SimpleStorage.methods.set(150).send();
-  //   let result = await SimpleStorage.methods.get().call();
-  //   assert.strictEqual(parseInt(result, 10), 150);
-  // });
-  //
-  // it("should have account with balance", async function() {
-  //   let balance = await web3.eth.getBalance(accounts[0]);
-  //   assert.ok(parseInt(balance, 10) > 0);
-  // });
 });
